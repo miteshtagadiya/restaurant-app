afb690902d0970c2281e419c5eca7824b27b8683
mitesh <miteshtagadiya@gmail.com>
2020-03-02 23:24:58 +0530

    Updates

---
+++ precache-manifest.9d79a1ad4ce810fc25d9a67d82275bb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e212f07eaa1151fdade6dd7e7dd296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ef636e52858ac96171",
+    "url": "/static/css/2.1d429f22.chunk.css"
+  },
+  {
+    "revision": "b900bb7f0cf7abc84838",
+    "url": "/static/css/main.b8dd7025.chunk.css"
+  },
+  {
+    "revision": "24ef636e52858ac96171",
+    "url": "/static/js/2.6959563b.chunk.js"
+  },
+  {
+    "revision": "b900bb7f0cf7abc84838",
+    "url": "/static/js/main.04ab2117.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dbc54e2617950cd6aceebde372ba5a0c",
+    "url": "/static/media/Logo.dbc54e26.png"
+  },
+  {
+    "revision": "41824335d7e23ce8726975debaa53581",
+    "url": "/static/media/login-banner.41824335.png"
+  }
+]);
