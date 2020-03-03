5619cafa8d3c0bfbd3b411e7d4d580cba759a201
mitesh <miteshtagadiya@gmail.com>
2020-03-03 23:58:46 +0530

    Updates

---
+++ precache-manifest.ec5061f7bcba2e10f7ada32aa11d7dec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f517d9dbdbcd4a49ffebf08bbe6c3a4",
+    "url": "https://miteshtagadiya.github.io/restaurant-app/index.html"
+  },
+  {
+    "revision": "f2a3e62201d68583486e",
+    "url": "https://miteshtagadiya.github.io/restaurant-app/static/css/2.afea7bfa.chunk.css"
+  },
+  {
+    "revision": "811754a3d2e6d1138e9c",
+    "url": "https://miteshtagadiya.github.io/restaurant-app/static/css/main.b8dd7025.chunk.css"
+  },
+  {
+    "revision": "f2a3e62201d68583486e",
+    "url": "https://miteshtagadiya.github.io/restaurant-app/static/js/2.9427812a.chunk.js"
+  },
+  {
+    "revision": "811754a3d2e6d1138e9c",
+    "url": "https://miteshtagadiya.github.io/restaurant-app/static/js/main.d9294eed.chunk.js"
+  },
+  {
+    "revision": "37b244fddcb3d542158f",
+    "url": "https://miteshtagadiya.github.io/restaurant-app/static/js/runtime~main.5c725a83.js"
+  },
+  {
+    "revision": "dbc54e2617950cd6aceebde372ba5a0c",
+    "url": "https://miteshtagadiya.github.io/restaurant-app/static/media/Logo.dbc54e26.png"
+  },
+  {
+    "revision": "41824335d7e23ce8726975debaa53581",
+    "url": "https://miteshtagadiya.github.io/restaurant-app/static/media/login-banner.41824335.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://github.com/miteshtagadiya/restaurant-app/precache-manifest.dcbf34c2e19912d649f6fff3685bdbd9.js"
+  "https://miteshtagadiya.github.io/restaurant-app/precache-manifest.ec5061f7bcba2e10f7ada32aa11d7dec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://github.com/miteshtagadiya/restaurant-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://miteshtagadiya.github.io/restaurant-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
