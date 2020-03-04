b2bf974bd46c3f6fc53eb9be862add767626a886
mitesh <miteshtagadiya@gmail.com>
2020-03-04 09:48:01 +0530

    App.js updated

--- src/Routes.js
+++ src/Routes.js
@@ -3,10 +3,9 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 
-
-export default () =>
+export default () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/login" exact component={Login} />
-
-  </Switch>;
+  </Switch>
+);
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom";
 import "./styles/global-styles.sass";
 import "./index.css";
 import App from "./components/index";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fab, fas);
-
-require("dotenv").config();
 
 const application = (
   <Router>
@@ -19,5 +16,3 @@ const application = (
 );
 
 ReactDOM.render(application, document.getElementById("root"));
-
-serviceWorker.unregister();
